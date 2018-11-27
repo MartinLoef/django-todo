3ca07dcb54492933f0fc32d1f18163ae877837a2
Martin Loef <loefmartin@gmail.com>
2018-11-27 15:01:13 +0000

    heroku and requirements.txt added

--- django_todo/django_todo/settings.py
+++ django_todo/django_todo/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -73,13 +74,13 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+DATABASES = {'default': dj_database_url.parse("postgres://mayfprtioaowkf:fb26bfdc755a8bc4451d6c64db712e5a68d7a7d9ad8f91fd26082bd618fad00e@ec2-54-247-119-167.eu-west-1.compute.amazonaws.com:5432/dffits1gqbeapr")}
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
